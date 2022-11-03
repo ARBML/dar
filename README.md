a434d2e089ea39a9f61873797dbee05a5fc810d3
zaidalyafeai <alyafey22@gmail.com>
2022-11-04 01:14:54 +0300

    update readme and do some refactoring

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # datasets_auto_wrapper
+
+The main script can be ran using the following commands 
+
+```bash
+python main.py
+```
+
+## Arguments 
+`--pal`: this argument is used for datasets that have labels as part of the foulder/file names. Upon 
+choosing this command the use is prompted to enter the `levels` which represents an integer that is used to extract the labels names from the full path. For example in `base_path/label/**.txt`, we define leveels = -2 which means that `label` will be given for all the files inside the label foulder. We can also define `base_path/label.txt` with levels = -2 which will remove the file extension `.txt`. 
+
+## Supported Links Types 
+Mainly after entering the dataset name, the user will be prompted to enter the dataset link. The user can either enter one link or mutliple links separated by comma. The supported links are the following 
+
+* `GitHub` The user can enter a link from GitHub without using `raw` as it will be converted automatically. The user can also either choose to enter a link to the repository in the following format `https://github.com/user/repo` which will download and extract the full directory or `https://github.com/user/repo/foulder` which will download all the files from that foulders as individual links. 
+* `Google Drive` The user can also enter a link from google drive in the following form `https://drive.google.com/file/d/id/view` which will directly download and extract the foulder to the local disk. 
+* `Direct links` The user can enter direct links for files i.e `https://domain/**.ext` with any extension and the file will be downloaded. Multilple lins can be concatenated using comma `https://domain/file1.ext,https://domain/file2.ext,...,https://domain/filen.ext`
+
+## Filteration
+The user can use glob structures to filter out some files from being used in the dataset. For example, the user is prompted with `Enter a glob structure: ` the user can enter something like `glob('foulder/**.txt')` which will only extract `.txt` files from the chosen `foulder`. The user will be prompted to enter multiple glob structures unless an empty `Enter` key is pressed. 
+
+## Supported file types
+Mainly the user will enter the file type when asked for `Enter the type:`, the supported file formats can be only one of the following 
+
+* `txt` mainly for reading the file as a whole or separated by lines. To differentiate between such options the user enters `set lines (y/n) ?` which if `y` the file will be separated into multiple lines or if `n` it will be read as a whole. 
+* `csv` this is used for files with sepcial separator, for example `.tsv` and `.txt` can be part of such family if a special separator is used. The program will try to guess the best separator but the user can also choose the separator using the command `Set a different Separator for {csv}`. The user can enter the separtors as `\t`,`,`,`;`,`|`, etc.
+* `json` can be used for dictionary like files. The use can choose `set lines (y/n) ?` as well which will decide whether to split the file by new lines or read the file as a whole. Also some datasets can have a parent dictionary for example `{'data':{'col1': [...], 'col2': [...]}}`, to support that the user can `set json key: ` whcih is `data` in this example. 
+* `xml` can be used for files that contain tags, for example `html` files as well. The user will be prompted to enter the column names for example `<s>this is good</s><l>positive</l> .....` then upon getting the prompt `enter the columns: ` the user can choose `s,l` such tags as columns. 
+* `wav` this is used for audio files like `.mp3,.wav` files. Upon choosing that the program will automatically create the following features as columns `{'path':path_to_file, 'audio':np.array(...)}`
+
+## File Processing 
+All the files will be processed using `pandas`. The user can modify some contents when prompted to 
+
+* `Enter rows to skip: ` this is used to skip some lines from the beginning of all the files. Mainly used to remove some metadata that is usually put as the header of files. The user can enter `0` which indicates that no lines will be skipped. 
+* `Data has a column? (y/n)` used to deal with files that have no column names, the user can set that `n` and enter the column names in the next step. 
+* `Enter new columns separated by comma:` used to creat different names for the columns or add columns if non exist. 
+* `Enter label column name:` used to choose the column that contains the labels. For example in sentiment anlaysis we will have the contents in a column as `positive` or `negative`. The user can put the name of that column to recognize that as the label. `datasets` will convert that to an integer which can be easily procssed by nlp model pipleines. 
+* `push to hub:` used to upload the dataset to hub. 
+
--- main.py
+++ main.py
@@ -1,178 +1,168 @@
 import os
 
-from attr import has
-import datasets
 from datasets.utils.download_manager import DownloadManager
 from datasets import load_dataset
 from utils import * 
-from config_code import get_config_code
 from class_code import get_class_code
 from constants import * 
 from features_code import get_features_code
 from generate_code import get_generate_code
 from imports_code import get_imports_code
 from split_code import get_split_code
-from glob import glob
 import argparse
-
 import os
-import sys
 
 # Create the parser
 my_parser = argparse.ArgumentParser()
 my_parser.add_argument('--pal','-paths_as_labels', action='store_true')
-my_parser.add_argument('--tal','-texts_as_labels', action='store_true')
 args = my_parser.parse_args()
-print(args.pal)
 
 dl_manager = DownloadManager()
 
 datasets_path = 'datasets'
-while True:
-    DATASET_NAME = input("Dataset Name: ") 
-    
-    main_class_code = get_class_code(DATASET_NAME)
-    URL = input("Enter Direct URL: ")
-    file_urls = convert_link(URL)
-    print(file_urls)
-    zipped = any([ext in file_urls[0] for ext in ['zip', 'rar', 'tar.gz', '7z', 'drive']])
-    label_names = None
+DATASET_NAME = input("Dataset Name: ") 
 
-    zip_base_dir = ''
-    alt_globs = []
+main_class_code = get_class_code(DATASET_NAME)
+URL = input("Enter Direct URL: ")
+file_urls = convert_link(URL)
+print(file_urls)
+zipped = any([ext in file_urls[0] for ext in ['zip', 'rar', 'tar.gz', '7z', 'drive']])
+label_names = None
 
-    level = None
-    download_data_path = {}
+zip_base_dir = ''
+alt_globs = []
 
-    if zipped:
-      try:
-        download_data_path = dl_manager.download_and_extract(file_urls)[0]
-      except:
-        file_urls = [URL]
-        download_data_path = dl_manager.download_and_extract(file_urls)[0]
-      zip_base_dir = download_data_path
-      extract_all(zip_base_dir)
-      print(zip_base_dir)
+level = None
+download_data_path = {}
 
-      download_data_path = {}
-      download_data_path['inputs'] = get_valid_files(zip_base_dir)
-      print(download_data_path)
+if zipped:
+  try:
+    download_data_path = dl_manager.download_and_extract(file_urls)[0]
+  except:
+    file_urls = [URL]
+    download_data_path = dl_manager.download_and_extract(file_urls)[0]
+  zip_base_dir = download_data_path
+  extract_all(zip_base_dir)
+  print(zip_base_dir)
 
-      alt_glob = ''
-      i = 1
-      while True:
-        alt_glob = input('Enter a glob structure: ')
-        if not alt_glob:
-          break
-        if len(alt_globs) == 0:
-          download_data_path['inputs'] = eval(alt_glob.replace("glob('", f"glob('{zip_base_dir}/"))
-          download_data_path['inputs'].sort()
-          alt_globs.append({'inputs':alt_glob})
-        else:
-          download_data_path[f'targets{i}'] = eval(alt_glob.replace("glob('", f"glob('{zip_base_dir}/"))
-          download_data_path[f'targets{i}'].sort()
-          alt_globs.append({f'targets{i}':alt_glob})
-          i += 1
+  download_data_path = {}
+  download_data_path['inputs'] = get_valid_files(zip_base_dir)
+  print(download_data_path)
 
-        
-
-      print(download_data_path)
-      if args.pal:
-        level = int(input('level for the labels: '))
-        if level == -1:
-          label_names = list(set([path.split('/')[-1] for path in download_data_path['inputs']]))
-          label_names = list(set([lbl.split('.')[-2] for lbl in label_names]))
-        else:
-          label_names = list(set([path.split('/')[level:level+1][0] for path in download_data_path['inputs']]))
-        print(label_names)
+  alt_glob = ''
+  i = 1
+  while True:
+    alt_glob = input('Enter a glob structure: ')
+    if not alt_glob:
+      break
+    if len(alt_globs) == 0:
+      download_data_path['inputs'] = eval(alt_glob.replace("glob('", f"glob('{zip_base_dir}/"))
+      download_data_path['inputs'].sort()
+      alt_globs.append({'inputs':alt_glob})
     else:
-      download_data_path['inputs'] = dl_manager.download(file_urls)
-    print(download_data_path)
+      download_data_path[f'targets{i}'] = eval(alt_glob.replace("glob('", f"glob('{zip_base_dir}/"))
+      download_data_path[f'targets{i}'].sort()
+      alt_globs.append({f'targets{i}':alt_glob})
+      i += 1
+
     
-    split_code = get_split_code(file_urls, download_data_path , zip_base_dir, alt_globs)
-    print(split_code)
 
-    type = input("Enter the type: ")
-    
-    lines = False 
-    json_key = ''
+  print(download_data_path)
+  if args.pal:
+    level = int(input('level for the labels: '))
+    if level == -1:
+      label_names = list(set([path.split('/')[-1] for path in download_data_path['inputs']]))
+      label_names = list(set([lbl.split('.')[-2] for lbl in label_names]))
+    else:
+      label_names = list(set([path.split('/')[level:level+1][0] for path in download_data_path['inputs']]))
+    print(label_names)
+else:
+  download_data_path['inputs'] = dl_manager.download(file_urls)
+print(download_data_path)
 
-    if type == 'json':
-      lines = True if input('set lines (y/n) ? ') == 'y' else False
-      json_key = input('set json key: ')
+split_code = get_split_code(file_urls, download_data_path , zip_base_dir, alt_globs)
+print(split_code)
 
-    if type == 'txt':
-      lines = True if input('set lines (y/n) ? ') == 'y' else False
+type = input("Enter the type: ")
 
-    df, best_sep = get_df(type, download_data_path, lines = lines, json_key=json_key)
+lines = False 
+json_key = ''
+
+if type == 'json':
+  lines = True if input('set lines (y/n) ? ') == 'y' else False
+  json_key = input('set json key: ')
+
+if type == 'txt':
+  lines = True if input('set lines (y/n) ? ') == 'y' else False
+
+df, best_sep = get_df(type, download_data_path, lines = lines, json_key=json_key)
+print(df.head())
+columns = [] 
+if type == 'xml':
+  columns = input('enter the columns: ').split(",")
+  df,_ = get_df(type, download_data_path, columns = columns)
+  print(df.head())
+
+if type in ['csv', 'tsv']:
+  print("Found best sep , ", best_sep)
+  best_sep = input(f"Set a different Separator for {type}")
+  if len(best_sep) > 0:
+    df, _ = get_df(type, download_data_path, 0, sep = best_sep, json_key=json_key)
     print(df.head())
-    columns = [] 
-    if type == 'xml':
-      columns = input('enter the columns: ').split(",")
-      df,_ = get_df(type, download_data_path, columns = columns)
-      print(df.head())
-    
-    if type in ['csv', 'tsv']:
-      print("Found best sep , ", best_sep)
-      best_sep = input(f"Set a different Separator for {type}")
-      if len(best_sep) > 0:
-        df, _ = get_df(type, download_data_path, 0, sep = best_sep, json_key=json_key)
-        print(df.head())
 
-    skiprows = 0
-    skiprows = int(input("Enter rows to skip: "))
-    if skiprows != 0:
-      df, _  = get_df(type, download_data_path, skiprows, sep = best_sep, lines = lines, json_key=json_key)
-      print(df.head())
-    columns = list(df.columns)
-    print(columns)
-    header = None if input("Data has a column? (y/n)") == 'n' else 0
-    new_columns = input("Enter new columns separated by comma").split(",")
-    if len(new_columns[0]) != 0:
-      columns = new_columns
-    df.columns= columns
-    set_label = True
-    label_column_name = ''
-    if not args.pal:
-      label_column_name = input("Enter label column name: ")
+skiprows = 0
+skiprows = int(input("Enter rows to skip: "))
+if skiprows != 0:
+  df, _  = get_df(type, download_data_path, skiprows, sep = best_sep, lines = lines, json_key=json_key)
+  print(df.head())
+columns = list(df.columns)
+print(columns)
+header = None if input("Data has a column (y/n)? ") == 'n' else 0
+new_columns = input("Enter new columns separated by comma: ").split(",")
+if len(new_columns[0]) != 0:
+  columns = new_columns
+df.columns= columns
+set_label = True
+label_column_name = ''
+if not args.pal:
+  label_column_name = input("Enter label column name: ")
 
-    if label_column_name != '':
-      label_names = list(set(df[label_column_name]))
-      print(label_names)
+if label_column_name != '':
+  label_names = list(set(df[label_column_name]))
+  print(label_names)
 
-    generate_code = ""
-    if args.pal:
-      generate_code += get_labels_from_path
-    if type == 'xml':
-      generate_code += xml_code
-    if type == 'txt':
-      generate_code += txt_code
-    if type == 'json':
-      generate_code += json_code
-    if type == 'wav':
-      generate_code += wav_code
-    
-    print(columns)
-    generate_code += get_generate_code(type, columns, label_names, label_column_name, skiprows = skiprows, use_labels_from_path = args.pal 
-                                      , sep = best_sep, header = header, lines = lines, json_key = json_key, level = level, alt_globs = alt_globs)
-    print(generate_code)
+generate_code = ""
+if args.pal:
+  generate_code += get_labels_from_path
+if type == 'xml':
+  generate_code += xml_code
+if type == 'txt':
+  generate_code += txt_code
+if type == 'json':
+  generate_code += json_code
+if type == 'wav':
+  generate_code += wav_code
 
-    if label_column_name != '':
-      if label_column_name in columns:
-        columns.remove(label_column_name)
+print(columns)
+generate_code += get_generate_code(type, columns, label_names, label_column_name, skiprows = skiprows, use_labels_from_path = args.pal 
+                                  , sep = best_sep, header = header, lines = lines, json_key = json_key, level = level, alt_globs = alt_globs)
+print(generate_code)
 
-    features_code = get_features_code(columns, label_names)
-    print(features_code)
-    extra_imports = []
-    if type == 'xml':
-      extra_imports = ['import re', 'from bs4 import BeautifulSoup']
-    import_code = get_imports_code(extra_imports)
-    code = import_code+main_class_code+features_code+split_code+generate_code
-    os.makedirs(f"{datasets_path}/{DATASET_NAME}", exist_ok = True)
-    open(f"{datasets_path}/{DATASET_NAME}/{DATASET_NAME}.py", "w").write(code)
-    dataset = load_dataset(f"{datasets_path}/{DATASET_NAME}")
-    print(dataset)
-    if input("push to hub: ") == 'y':
-      print('pushing to the hub') 
-      dataset.push_to_hub(f"arbml/{DATASET_NAME}")
-    break
+if label_column_name != '':
+  if label_column_name in columns:
+    columns.remove(label_column_name)
+
+features_code = get_features_code(columns, label_names)
+print(features_code)
+extra_imports = []
+if type == 'xml':
+  extra_imports = ['import re', 'from bs4 import BeautifulSoup']
+import_code = get_imports_code(extra_imports)
+code = import_code+main_class_code+features_code+split_code+generate_code
+os.makedirs(f"{datasets_path}/{DATASET_NAME}", exist_ok = True)
+open(f"{datasets_path}/{DATASET_NAME}/{DATASET_NAME}.py", "w").write(code)
+dataset = load_dataset(f"{datasets_path}/{DATASET_NAME}")
+print(dataset)
+if input("push to hub: ") == 'y':
+  print('pushing to the hub') 
+  dataset.push_to_hub(f"arbml/{DATASET_NAME}")
