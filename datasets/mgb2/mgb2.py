f120a1a0df649900ea6e61f69e84d5a1c7dda70b
zaidalyafeai <alyafey22@gmail.com>
2022-12-04 20:57:43 +0300

    add mgb2

---
+++ datasets/mgb2/mgb2.py
@@ -0,0 +1,51 @@
+import os
+import pandas as pd 
+import datasets
+from glob import glob
+import zipfile
+
+class mgb2(datasets.GeneratorBasedBuilder):
+	def _info(self):
+		return datasets.DatasetInfo(features=datasets.Features({'text':datasets.Value('string'),'path':datasets.Value('string'),'audio':datasets.Audio(sampling_rate=16_000)}))
+
+	def extract_all(self, dir):
+		zip_files = glob(dir+'/**/**.zip', recursive=True)
+		for file in zip_files:
+			with zipfile.ZipFile(file) as item:
+				item.extractall('/'.join(file.split('/')[:-1])) 
+
+
+	def get_all_files(self, dir):
+		files = []
+		valid_file_ext = ['txt', 'csv', 'tsv', 'xlsx', 'xls', 'xml', 'json', 'jsonl', 'html', 'wav', 'mp3']
+		for ext in valid_file_ext:
+			files += glob(f"{dir}/**/**.{ext}", recursive = True)
+		return files
+
+	def _split_generators(self, dl_manager):
+		url = [os.path.abspath(os.path.expanduser(dl_manager.manual_dir))]
+		downloaded_files = url
+		return [datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={'filepaths':{'inputs':sorted(glob(downloaded_files[0]+'/*.txt')),'targets1':sorted(glob(downloaded_files[0]+'/*.wav')),} })]
+
+
+	def read_wav(self, filepath):
+		if filepath.endswith('.wav') or filepath.endswith('.mp3'):
+			raw_data = {'filepath':[filepath], 'audio':[filepath]}
+		else:
+			raw_data = {'text':[open(filepath).read()]}
+		return pd.DataFrame(raw_data)
+
+	def _generate_examples(self, filepaths):
+		_id = 0
+		for i,filepath in enumerate(filepaths['inputs']):
+			df = self.read_wav(filepath)
+			dfs = [df] 
+			dfs.append(self.read_wav(filepaths['targets1'][i]))
+			df = pd.concat(dfs, axis = 1)
+			if len(df.columns) != 3:
+				continue
+			df.columns = ['text', 'path', 'audio']
+			for _, record in df.iterrows():
+				yield str(_id), {'text':record['text'],'path':record['path'],'audio':record['audio']}
+				_id += 1 
+
